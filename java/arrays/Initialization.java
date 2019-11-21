8fb7f195bb4c8328296079492907089af3cbb3f6
Administrator <admin@example.com>
2019-11-21 11:25:55 -0700

    fix: added comments

--- java/arrays/Initialization.java
+++ java/arrays/Initialization.java
@@ -4,7 +4,7 @@ public class Initialization {
     public static void main(String args[]) {
         int a[] = { 2, 4, 6, 8, 10 };
         
-        int[] b = { 1, 3, 5, 7, 9 };
+        int[] b = { 1, 3, 5, 7, 9 }; // this array is created in stack
 
         String[] c = { "1", "2" , "3" , "4" , "5" };
 
